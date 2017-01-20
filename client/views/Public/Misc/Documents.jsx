20f6a22ae67015091a1add5dade6fb4d5d5b5fa7
Dante Nolasco <dnolascodante@gmail.com>
2017-01-20 20:33:20 -0200

    fixed file upload

--- client/views/Public/Misc/Documents.jsx
+++ client/views/Public/Misc/Documents.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Card, CardActions, CardHeader, FlatButton, CardText, TextField, Dialog, CardTitle, } from 'material-ui';
 
 PublicUploadDocument = React.createClass({
