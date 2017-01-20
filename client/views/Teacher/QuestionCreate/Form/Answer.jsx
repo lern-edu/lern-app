243a54217a63d600c1200f6d35986d8e2b4e0ac5
Dante Nolasco <dnolascodante@gmail.com>
2017-01-20 20:13:18 -0200

    questions and partial createQuestion on material-ui (minor fix)

--- client/views/Teacher/QuestionCreate/Form/Answer.jsx
+++ client/views/Teacher/QuestionCreate/Form/Answer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import DropdownMenu from 'material-ui/DropdownMenu';
-import TextField from 'material-ui/TextField';
-import MenuItem from 'material-ui/MenuItem';
+import { DropdownMenu, MenuItem, TextField } from 'material-ui';
 
 TeacherQuestionCreateFormAnswer = React.createClass({
 
