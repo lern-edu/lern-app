439416f1083c0dc7365c3af4f8883818554bafdd
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-02-04 02:22:32 -0200

    add tags validator

--- packages/se-model/validators/tags.js
+++ packages/se-model/validators/tags.js
@@ -2,7 +2,7 @@ Astro.createValidator({
   name: 'Tags',
   validate(values) {
     // TODO tagable can't verify like this bacause will bug automatic tests
-    return true;
+    // return true;
     return _.get(this, '_isNew') ? !_.isEmpty(_.get(this, 'tags')) : true;
   },
 });
