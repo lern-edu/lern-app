560a1eb39eec36863f20fc429e88e8a5a14448ce
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-02-08 18:36:50 -0200

    change tags validator to validate only astro isNew

--- packages/se-model/validators/tags.js
+++ packages/se-model/validators/tags.js
@@ -1,6 +1,6 @@
 Astro.createValidator({
   name: 'Tags',
-  validate(values) {
+  validate(values, e1, e2) {
     // TODO tagable can't verify like this bacause will bug automatic tests
     // return true;
     return _.get(this, '_isNew') ? !_.isEmpty(_.get(this, 'tags')) : true;
