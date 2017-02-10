1198442115d12efbf6a6394e50b0e416aee0a15d
Dante Nolasco <dnolascodante@gmail.com>
2017-02-10 15:23:58 -0200

    created SchoolUsers view

---
+++ client/views/School/Users/Container.jsx
@@ -0,0 +1,10 @@
+// // Libs
+// import { createContainer } from 'meteor/react-meteor-data';
+// import { Meteor } from 'meteor/meteor';
+//
+// // Views
+// import SchoolUsersView from './View.jsx';
+//
+// SchoolUsers = createContainer(({ params }) => {
+//
+// }, SchoolUsersView);
---
+++ client/views/School/Users/View.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/*const*/ SchoolUsers = React.createClass({
+
+  /* Render
+  */
+
+  render() {
+    const { ready, courses, subjects } = this.props;
+
+    return (
+      <div className='ui container'>
+        <Layout.Bar title='Alunos' />
+        <a href={FlowRouter.path('SchoolCourses')}>Que tal gerenciar seus cursos?</a>
+        <a href={FlowRouter.path('SchoolUsers')}>Que tal gerenciar alunos e professores?</a>
+        {/* <SchoolHomeToolbar /> */}
+      </div>
+    );
+  },
+});
+
+// export default SchoolUsersView;
