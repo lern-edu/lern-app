e99252f83775c4515220cf242fc4aa57452922ef
Andre <andredornas299@gmail.com>
2017-02-20 21:39:53 -0300

    show question

---
+++ client/views/Student/TestAttempt/Page/Content/index.jsx
@@ -0,0 +1,46 @@
+// Libs
+import React from 'react';
+import { Paper, FlatButton } from 'material-ui';
+import { Editor, EditorState, convertFromRaw } from 'draft-js';
+
+import StudentTestAttemptPageContentQuestionView from './Question.jsx';
+
+StudentTestAttemptPageContent = React.createClass({
+  mixins: [AstroForm()],
+
+  // Render
+
+  render() {
+    const { scored, questions } = this.props;
+    const text = this.doc.get('text');
+
+    console.log('ABCD');
+    console.log(this.props);
+
+    return (
+      <div style={{ padding: 15 }}>
+
+        {_.get({
+          text: <Editor
+              readOnly={true}
+              editorState={text && EditorState.createWithContent(
+                convertFromRaw(text))} />,
+          link: <a>{this.doc.get('link')}</a>,
+          title: <h4>{this.doc.get('title')}</h4>,
+          image: <PublicContentShowImage
+            form={this}
+            imageId={this.doc.get('image')} />,
+          question: <StudentTestAttemptPageContentQuestionView
+            score={this.doc.get('score')}
+            form={this}
+            questionId={this.doc.get('question')}
+            scored={scored}
+            question={_.find(questions, { _id: this.doc.get('question') })}
+            {...this.props} />,
+        }, this.doc.get('type'))}
+
+      </div>
+    );
+  },
+
+});
