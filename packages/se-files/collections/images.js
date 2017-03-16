647401ea33b66a9621b49e5330f79a40852b0676
Andre <andredornas299@gmail.com>
2017-03-16 20:36:05 -0300

    GRID.Images collection name, instead of FS.Images

--- packages/se-files/collections/images.js
+++ packages/se-files/collections/images.js
@@ -14,7 +14,7 @@ if (Meteor.isServer) {
 
 FS.Images = new FilesCollection({
   storagePath: 'assets/app/uploads/images',
-  collectionName: 'FS.Images',
+  collectionName: 'GRID.Images',
   onBeforeUpload: function (file) {
     if (file.size <= (50 * 1024 * 1024) && /png|jpg|jpeg/i.test(file.ext)) {
       return true;
