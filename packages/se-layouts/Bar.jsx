4f635e3114ab73f83996d07930fdc66f11255b19
Dante Nolasco <dnolascodante@gmail.com>
2017-03-24 15:33:29 -0300

    removed leftover code

--- packages/se-layouts/Bar.jsx
+++ packages/se-layouts/Bar.jsx
@@ -2,63 +2,6 @@ import React from 'react';
 import { AppBar, FlatButton, IconButton, FontIcon } from 'material-ui';
 import { Avatar, MenuItem, IconMenu, Divider } from 'material-ui';
 
-const Settings = React.createClass({
-
-  /* Get Context
-  */
-
-  contextTypes: {
-    user: React.PropTypes.object,
-  },
-
-  // Render
-
-  render() {
-    const { user } = this.context;
-    const { disableActions } = this.props;
-    const profilePic = _.get(user, 'profile.profilePic');
-    const name = _.get(user, 'profile.name');
-
-    return (
-      !user ? <FlatButton href={FlowRouter.path('PublicLogin')} label='Login' /> :
-      <IconMenu {..._.omit(this.props, ['disableActions'])}
-        iconButtonElement={
-          <IconButton>{profilePic ? <Avatar src={profilePic} /> :
-            <Avatar size={32}>{_.first(name)}</Avatar>}</IconButton>}
-        targetOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-        anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }} >
-        <MenuItem primaryText={name} disabled={true} />
-    });    <Divider />
-        {disableActions ? undefined : <MenuItem
-          primaryText='Configurações'
-          rightIcon={<FontIcon className='material-icons' >settings</FontIcon>}
-          onClick={event => event.stopPropagation() ||
-            FlowRouter.go(user.getSettingsRoute())} />
-        }
-        {user.getRoles().length <= 1 ? undefined : <MenuItem
-          primaryText='Ver como'
-          rightIcon={<FontIcon className='material-icons' >remove_red_eye</FontIcon>}
-          leftIcon={<FontIcon className='material-icons' >keyboard_arrow_left</FontIcon>}
-          menuItems={_.map(user.getRoles(), r => <MenuItem
-            key={r}
-            primaryText={i18n.__(`UserRoles.${r}`)}
-            onClick={() => this.handleRoleChange(r)} />)
-          } />
-        }
-        <MenuItem
-          primaryText='Fale conosco'
-          rightIcon={<FontIcon className='material-icons' >email</FontIcon>}
-          onClick={event => event.stopPropagation() ||
-            FlowRouter.go('PublicContact')} />
-        <MenuItem
-          primaryText='Sair'
-          rightIcon={<FontIcon className='material-icons' >exit_to_app</FontIcon>}
-          onClick={event => event.stopPropagation() || Meteor.logout()} />
-      </IconMenu>
-    );
-  },
-});
-
 Layout.Bar = React.createClass({
   propTypes: {
     crumbs: React.PropTypes.arrayOf(
