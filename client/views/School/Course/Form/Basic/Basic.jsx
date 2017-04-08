364edf60722fe8d75e19622c10a2092c85c8f5bb
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-04-07 21:13:34 -0300

    pass course to PublicContentCreate

--- client/views/School/Course/Form/Basic/Basic.jsx
+++ client/views/School/Course/Form/Basic/Basic.jsx
@@ -37,7 +37,9 @@ const SchoolCourseForm = React.createClass({
                 field='info'
                 schema={Courses.ContentSchema}
                 contentTypes={ContentTypes}
-                form={form} />
+                form={form}
+                course={form.doc.get('_id')}
+              />
             </div>
           </div>
 
