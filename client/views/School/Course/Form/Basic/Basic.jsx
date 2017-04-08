d2109c7f2304fb818905d7b3aa1da16bb499c405
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-04-07 21:14:01 -0300

    same last commit

--- client/views/School/Course/Form/Basic/Basic.jsx
+++ client/views/School/Course/Form/Basic/Basic.jsx
@@ -51,7 +51,8 @@ const SchoolCourseForm = React.createClass({
                   field='info'
                   form={form}
                   index={i}
-                  doc={s} />
+                  doc={s}
+                />
             </div>)}
           </div>
 
