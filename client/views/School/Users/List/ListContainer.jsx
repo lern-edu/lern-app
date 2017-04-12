9e1b24bee2506c95e515cceae3ebfd2c8ed1d896
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-04-12 11:37:14 -0300

    update view to allow set teacher

--- client/views/School/Users/List/ListContainer.jsx
+++ client/views/School/Users/List/ListContainer.jsx
@@ -6,12 +6,11 @@ import SchoolUsersListView from './List.jsx';
 export default SchoolUsersList = createContainer((props) => {
   const userId = Meteor.userId();
   const handles = {
-    students: Meteor.subscribe('SchoolStudents'),
-  };
-  const data = {
-    ready: _.mapValues(handles, h => h.ready()),
-    students: Fetch.School(userId).students().fetch(),
+    users: Meteor.subscribe('SchoolUsers'),
   };
 
-  return data;
+  return {
+    ready: _.mapValues(handles, h => h.ready()),
+    users: Fetch.School(userId).users().fetch(),
+  };
 }, SchoolUsersListView);
