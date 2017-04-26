9853dc94a6ba55d86a5eab61cd7f68ea4be13230
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-04-26 12:22:47 -0300

    add publication to get post

--- packages/se-publications/server/user.js
+++ packages/se-publications/server/user.js
@@ -12,3 +12,27 @@ Helpers.Publications({ type: 'plain', prefix, protect }, {
     return Fetch.General.users(_.union(schools, [school]));
   },
 });
+
+Helpers.Publications({ type: 'composite', prefix, protect }, {
+  Post({ postId }={}, { users, course }={}) {
+    const { userId } = this;
+    return {
+      find() {
+        return Fetch.General.posts(postId);
+      },
+
+      children: [
+        {
+          find(post) {
+            return course && post.findCourse();
+          },
+        }, {
+          find(post) {
+            return users && post.findUsers();
+          },
+        },
+      ],
+    };
+  },
+
+});
