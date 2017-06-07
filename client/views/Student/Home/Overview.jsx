c12c1b6fa78c45e00c023a749e618a81bf4e6996
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-06-07 18:37:53 -0300

    add search button on home student

--- client/views/Student/Home/Overview.jsx
+++ client/views/Student/Home/Overview.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Screen } from 'meteor/lsunsi:se-layouts';
-import { Card, CardMedia, CardTitle, CardActions, FlatButton, Paper, Styles } from 'material-ui';
-import { List, ListItem, Subheader, TextField } from 'material-ui';
+import { Card, CardMedia, CardTitle, CardActions, Paper } from 'material-ui';
+import { List, ListItem, Subheader, TextField, FlatButton } from 'material-ui';
 import { blue700 } from 'material-ui/styles/colors';
 
 StudentHomeOverview = React.createClass({
@@ -17,7 +17,11 @@ StudentHomeOverview = React.createClass({
 
   handlePressEnter(event) {
     if (event.keyCode == 13)
-      FlowRouter.go('StudentCourseIngress', {}, { alias: this.state.courseKey });
+      this.handleSearch();
+  },
+
+  handleSearch() {
+    FlowRouter.go('StudentCourseIngress', {}, { alias: this.state.courseKey });
   },
 
   handleKey(event, courseKey) {
@@ -38,8 +42,10 @@ StudentHomeOverview = React.createClass({
            <div className='ui items'>
               <div className='item'>
                 <div className='ui medium image'>
-                  <img src='/images/steps-students/seta.svg'
-                   style={{ width: '300px', padding: '10px' }} />
+                  <img
+                    src='/images/steps-students/seta.svg'
+                    style={{ width: '300px', padding: '10px' }}
+                  />
                 </div>
                <div className='content' style={{ padding: 20 }}>
                  <Paper style={{ width: 293 }}>
@@ -47,6 +53,7 @@ StudentHomeOverview = React.createClass({
                      <Subheader>Buscar Curso</Subheader>
                      <ListItem
                        disabled={true}
+                       style={{ padding: '0px 16px 0px 16px' }}
                        children={
                           <TextField
                             key='text'
@@ -57,25 +64,39 @@ StudentHomeOverview = React.createClass({
                           />
                         }
                      />
+                     <ListItem
+                       disabled={true}
+                       style={{ padding: '0px 16px 0px 16px' }}
+                       children={
+                          <FlatButton
+                            key='button'
+                            secondary={true}
+                            label='BUSCAR'
+                            onTouchTap={this.handleSearch}
+                          />
+                        }
+                     />
                    </List>
                 </Paper>
               </div>
             </div>
           </div>
-          <div className='ui centered grid' style={{ margin: '0 !important' }}>
-             {_.map(courses, course =>
-               <Card className='fourteen wide mobile six wide tablet four wide computer column'
-                  key={course._id} style={{ margin: '20px' }}>
-                 <CardTitle
-                   title={course.name} />
-                 <CardActions>
-                   <FlatButton
-                     primary={true}
-                     href={FlowRouter.path('StudentCourseShow', { courseId: course._id })}
-                     label='Entrar' />
-                 </CardActions>
-               </Card>
-           )}
+          <div className='ui centered grid' style={{ margin: 0 }}>
+             {
+               _.map(courses, course =>
+                 <Card className='fourteen wide mobile six wide tablet four wide computer column'
+                    key={course._id} style={{ margin: '20px' }}>
+                   <CardTitle
+                     title={course.name} />
+                   <CardActions>
+                     <FlatButton
+                       secondary={true}
+                       href={FlowRouter.path('StudentCourseShow', { courseId: course._id })}
+                       label='Entrar' />
+                   </CardActions>
+                 </Card>
+             )
+           }
         </div>
       </div>
     );
