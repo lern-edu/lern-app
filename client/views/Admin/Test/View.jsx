57739b611269c87021f6053b5f370e6fa07626b3
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-06-20 18:47:42 -0300

    bugfix data reactive

--- client/views/Admin/Test/View.jsx
+++ client/views/Admin/Test/View.jsx
@@ -13,21 +13,22 @@ export default class AdminTestView extends React.Component {
   */
 
   render() {
-    const { ready, test, subjects } = this.data;
+    const { ready, test, subjects } = this.props;
     return (
       <div className='ui container'>
 
         <Layout.Bar
           title={_.get(test, 'name')}
-          crumbs={[{ label: 'Testes', path: 'AdminTests' }]} />
+          crumbs={[{ label: 'Testes', path: 'AdminTests' }]}
+        />
 
         <Paper>
           {!_.every(ready) ? <LinearProgress/> : [
-            <AdminTestHeader key='header' {...this.data} />,
+            <AdminTestHeader key='header' {...this.props} />,
             <Divider key='d0'/>,
             <AdminTestForm key='question' doc={test} subjects={subjects} />,
             <Divider key='d1'/>,
-            <AdminTestActions key='actions' {...this.data} />,
+            <AdminTestActions key='actions' {...this.props} />,
           ]}
         </Paper>
 
